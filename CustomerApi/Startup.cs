5b3b8e601472cdfc18b784b3243fcc42803298fb
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2020-03-04 10:20:57 +0100

    Docker support

--- CustomerApi/Startup.cs
+++ CustomerApi/Startup.cs
@@ -57,7 +57,7 @@ namespace CustomerApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
